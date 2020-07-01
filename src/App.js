33d8a4a1901fc5880d9398e3c00d85a92ce3f544
Symbol <Symbolistic@live.com>
2020-07-01 11:05:59 -0400

    pushing to get some help with react's css issues

--- src/App.js
+++ src/App.js
@@ -6,21 +6,22 @@ import './App.css';
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
   const [wikiData, setWikiData] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
-
 
   const search = event => {
-    event.preventDefault();
     let url = "https://en.wikipedia.org/w/api.php";
 
     let params = {
       action: "query",
-      list: "search",
-      srsearch: searchQuery,
+      prop: "extracts",
+      generator: "search",
+      exintro: '1',
+      explaintext: '1',
+      exsentences: '1',
+      gsrsearch: searchQuery,
       format: "json"
     }
 
-    
+    if (event.key === "Enter") {
       url = url + "?origin=*";
 
       Object.keys(params).forEach((key) => url += '&' + key + '=' + params[key]);
@@ -29,42 +30,37 @@ function App() {
         .then(res => res.json())
         .then(response => {
           console.log("Exists! Success!");
-          console.log(typeof response)
-          setWikiData(response.query.search);
-          setSearchQuery('')
-          console.log(wikiData.length)
-          setIsLoaded(false);
+          //console.log(response.query.pages)
+          setSearchQuery('');
+          setWikiData(Object.entries(response.query.pages).map(val => val[1]));
         })
         .catch(error => console.log(error));
-    
+    }
   }
 
   return (
     <div className="App">
+      <h1 id="title">Wikipedia Viewer</h1>
       <div className="search-box">
-        <form onSubmit={search}>
-            <input 
-              type="text" 
-              className="search-bar" 
-              placeholder="Search..."
-              onChange={e => setSearchQuery(e.target.value)}
-              value={searchQuery}
-              
-              />
-
-              <input type="submit" value="Submit" />
-        </form>
+          <input 
+            type="text" 
+            className="search-bar" 
+            placeholder="Search..."
+            onChange={e => setSearchQuery(e.target.value)}
+            value={searchQuery}
+            onKeyPress={search}
+            />
         </div>
           <div>
-            {(wikiData.length > 0 && isLoaded === false) ? (
+            {(wikiData.length > 0) ? (
               wikiData.map((val) => {
-                console.log(val)
+
                 return (
-                  <div>
+                  <div key={val.pageid}>
                     <h2>{val.title}</h2>
-                    <p>{val.snippet}</p>
+                    <p>{val.extract}</p>
                   </div>
-                )       
+                );
               })
             ) : ('')}
           </div>
