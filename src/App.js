e76ed014f1dbc91d2bcf404eea86b54dc6c16768
Symbol <Symbolistic@live.com>
2020-06-30 22:11:14 -0400

    first commit, uploading to get some help on a rendering issue with react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [wikiData, setWikiData] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+
+  const search = event => {
+    let url = "https://en.wikipedia.org/w/api.php";
+
+    let params = {
+      action: "query",
+      list: "search",
+      srsearch: searchQuery,
+      format: "json"
+    }
+
+    if (event.key === "Enter") {
+      url = url + "?origin=*";
+
+      Object.keys(params).forEach((key) => url += '&' + key + '=' + params[key]);
+
+      fetch(url)
+        .then(res => res.json())
+        .then(response => {
+          console.log("Exists! Success!");
+          console.log(typeof response)
+          setWikiData(response.query.search);
+          setSearchQuery('')
+          console.log(wikiData.length)
+          setIsLoaded(false);
+        })
+        .catch(error => console.log(error));
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-box">
+          <input 
+            type="text" 
+            className="search-bar" 
+            placeholder="Search..."
+            onChange={e => setSearchQuery(e.target.value)}
+            value={searchQuery}
+            onKeyPress={search}
+            />
+        </div>
+          <div>
+            {(wikiData.length > 0 && isLoaded === false) ? (
+              wikiData.map((val) => {
+                console.log(val)
+                return (
+                  <div>
+                    <h2>{val.title}</h2>
+                    <p>{val.snippet}</p>
+                  </div>
+                )       
+              })
+            ) : ('')}
+          </div>
     </div>
   );
 }
