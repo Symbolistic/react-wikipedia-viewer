8eb10e7cfd5e112e8c816f201623a37f7e467cb7
Symbol <Symbolistic@live.com>
2020-07-01 14:22:17 -0400

    Updates

---
+++ precache-manifest.efb50d022b7c67207f5e5110d6850a02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b8b060cf0f0d1299172f1a13fa2314",
+    "url": "/react-wikipedia-viewer/index.html"
+  },
+  {
+    "revision": "4256f8b60cdce865e8db",
+    "url": "/react-wikipedia-viewer/static/css/main.e32b635c.chunk.css"
+  },
+  {
+    "revision": "fb8fcf52f5baef161c5c",
+    "url": "/react-wikipedia-viewer/static/js/2.5b71efd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-wikipedia-viewer/static/js/2.5b71efd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4256f8b60cdce865e8db",
+    "url": "/react-wikipedia-viewer/static/js/main.9033718a.chunk.js"
+  },
+  {
+    "revision": "43d17ef92b0348dbafbf",
+    "url": "/react-wikipedia-viewer/static/js/runtime-main.ebb8af26.js"
+  }
+]);
